b7504bf085042753f6270827dac1364810544e60
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2023-04-27 01:29:15 +0530

    Added Model for SensorData

--- Cropprediction/models.py
+++ Cropprediction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Valuesfromsensor(models.Model):
+    temperature = models.FloatField(db_column='TEMPERATURE', blank=True, null=True)  # Field name made lowercase.
+    humidity = models.FloatField(db_column='HUMIDITY', blank=True, null=True)  # Field name made lowercase.
+    moisture = models.FloatField(db_column='MOISTURE', blank=True, null=True)  # Field name made lowercase.
+    ph = models.FloatField(db_column='Ph', blank=True, null=True)  # Field name made lowercase.
+    eventprocessedutctime = models.DateTimeField(db_column='EventProcessedUtcTime', blank=True, null=False,primary_key=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'VALUESFROMSENSOR'
