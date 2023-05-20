1df297b9c54b641370cc842a6888f5b3d0afb857
gkvishal7 <gkvishal7@gmail.com>
2023-05-20 17:29:37 +0530

    created models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+    phone_no = models.CharField(max_length=11)
+    password = models.CharField(max_length=30)
+    confirm_password = models.CharField(max_length=30)
+
+
