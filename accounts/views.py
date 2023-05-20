ceaf54ca582cfed43cef38579354f533f8100340
gkvishal7 <gkvishal7@gmail.com>
2023-05-20 17:30:14 +0530

    added login and register functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render,redirect
-
+from .models import User as U
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User as Usr
 # Create your views here.
+
 
 def login(request):
     if request.method =="POST":
         username = request.POST['username']
-        password = request.POST['password']
+        passwd = request.POST['password']
 
         print(username)
-        print(password)
+        print(passwd)
 
-        
-
-        return redirect("/cropprediction")
+        user = authenticate(request,email=username,password=passwd)
+        print(user)
+        if user is not None:
+            login(request,user)
+            print("successs")
+            return redirect("www.google.com")
+        else:
+            redirect("/")
 
     return render(request,'accounts/login.html')
 
@@ -30,6 +38,12 @@ def register(request):
         print(passwd)
         print(confirm_passwd)
 
-        return redirect('/login')
+        user_details = U(name=full_name,email=email_id,phone_no=phone_number,password = passwd,confirm_password=confirm_passwd)
+        user_details.save()
+
+        user = Usr(email=email_id,password=passwd)
+        user.save()
+
+        return redirect('/')
 
     return render(request,'accounts/register.html')
